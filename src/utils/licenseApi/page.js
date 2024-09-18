981a90422bf93f43be7ad48db98800d2a2698c23
gauravkumarit8 <gaurav.kumarit8@gmail.com>
2024-09-18 22:12:39 +0530

    Added the authorization for license api

--- src/utils/licenseApi/page.js
+++ src/utils/licenseApi/page.js
@@ -1,11 +1,13 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL;
 export async function issueLicenseToUser(userId) {
     try {
+      const token = localStorage.getItem('authToken');
         const response = await fetch(
           `${BASE_URL}/api/license/issue?userId=${userId}`,  // Pass userId as a query parameter
           {
             method: "POST",
             headers: {
+              'Authorization': `Bearer ${token}`,
               "Content-Type": "application/json",
             },
           }
@@ -25,11 +27,13 @@ export async function issueLicenseToUser(userId) {
 
 export async function getUserLicenseDetails(userId) {
     try {
+      const token = localStorage.getItem('authToken');
         const response = await fetch(
           `${BASE_URL}/api/license/user/${userId}`,
           {
             method: "GET",
             headers: {
+              'Authorization': `Bearer ${token}`,
               "Content-Type": "application/json",
             },
           }
@@ -47,11 +51,13 @@ export async function getUserLicenseDetails(userId) {
 
 export async function getLicenseDetails(licenseId) {
     try {
+      const token = localStorage.getItem('authToken');
         const response = await fetch(
           `${BASE_URL}/api/license/${licenseId}`,
           {
             method: "GET",
             headers: {
+              'Authorization': `Bearer ${token}`,
               "Content-Type": "application/json",
             },
           }
@@ -70,11 +76,13 @@ export async function getLicenseDetails(licenseId) {
 
 export async function updateLicenseDetails(licenseDetails) {
     try {
+      const token = localStorage.getItem('authToken');
         const response = await fetch(
           `${BASE_URL}/api/license/update`,
           {
             method: "PATCH",
             headers: {
+              'Authorization': `Bearer ${token}`,
               "Content-Type": "application/json",
             },
             body: JSON.stringify(licenseDetails)
@@ -94,11 +102,13 @@ export async function updateLicenseDetails(licenseDetails) {
 
 export async function deleteLicenseDetails(licenseId) {
     try {
+      const token = localStorage.getItem('authToken');
         const response = await fetch(
           `${BASE_URL}/api/license/delete/${liceseId}`,
           {
             method: "DELETE",
             headers: {
+              'Authorization': `Bearer ${token}`,
               "Content-Type": "application/json",
             },
           }
